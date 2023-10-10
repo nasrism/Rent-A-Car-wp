8441d8a7c7aa5724e9367970a730a53219bca969
nasrism <nasrullahismail010@gmail.com>
2023-10-11 02:28:42 +0800

    good news
    finally understand how to use wp_query

--- search.php
+++ search.php
@@ -10,18 +10,69 @@ get_template_part('filter-function');?>
 <div class="items-container sc-r-c">
    
     <?php 
-    if(have_posts()):
-        while (have_posts()): the_post();
+
+        $rent_price = '';
+        $type = '';
+        $brand = '';
+
+        if(isset($_GET['rent-price']) && !empty($_GET['rent-price'])){
+            $rent_price = $_GET['rent-price'];
+        }
+        
+        if(isset($_GET['type']) && !empty($_GET['type'])){
+            $type = $_GET['type'];
+        }
+        
+        if(isset($_GET['brand']) && !empty($_GET['brand'])){
+            $brand = $_GET['brand'];
+        }
+
+        $args = array(
+            'post_type' => 'car',
+            'post_per_page' => -1,
+            'orderby' => 'rand',
+            'meta_query' => array(
+                array(
+                    'key' => 'brand',
+                    'value' => $brand,
+                    'compare' => 'LIKE',
+                ),
+                array(
+                    'key' => 'type',
+                    'value' => $type,
+                    'compare' => 'LIKE',
+                ),
+                array(
+                    'key' => 'rent-price',
+                    'value' => $rent_price,
+                    'compare' => 'LIKE',
+                ),
+
+            )
+            ); 
+
+        $query = new WP_Query($args); 
+        if($query -> have_posts()):
+        while ($query -> have_posts()): $query -> the_post();
+        
+        // if(have_posts()):
+        // while (have_posts()): the_post();
+
         ?>
-        <div class="sc-r">
+        <a href="<?php echo get_permalink() ?>" class="sc-r">
             <img class="thumbnail" src=<?php the_post_thumbnail();
             echo '<h2>';
-                echo '<a href="' . get_permalink() . '">';
                 the_title();
-                echo '</a>';
             echo '</h2>';
-            the_content();?>
-        </div>
+            echo '<h5>';
+            echo 'RM';
+            the_field('rent-price-ph'); 
+            echo '</h5>'; 
+            echo '<p>';
+            echo 'per hour';
+            echo '</p>';
+            ?>
+        </a>
         <?php
         endwhile;
         else:
--- searchform.php
+++ searchform.php
@@ -5,6 +5,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@200;700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/75b710a8ef.js" crossorigin="anonymous"></script>
 
+
 <form role="search" method="get" id="searcher" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <div class="filter-search" style="scale:0; opacity:0;">
     
@@ -37,18 +38,23 @@
               
             <div class="type fil-list sel-fil">
                 <h5>Car type</h5>
-                <select name="type" id="car-type">
+                <select name="type" id="type">
                     <option value="">Any</option>
                     <?php 
                         $car_types = array(
                             'suv' => 'SUV',
-                            'sedan' => 'Sedan',
-                            'truck' => 'Truck',
+                            'compact' => 'Compact',
+                            'pickup' => 'Pickup Truck',
                             'convertible' => 'Convertible',
+                            'coupe' => 'Coupe',
+                            'hatchback' => 'Hatchback',
+                            'crossover' => 'Crossover',
+                            'minivan' => 'Minivan'
+
                         );
 
                         foreach ($car_types as $value => $label) {
-                            $selected = ($_GET['car-type'] == $value) ? 'selected' : '';
+                            $selected = (isset($_GET['type']) && $_GET['type'] == $value) ? 'selected' : '';
                             echo '<option value="' . esc_attr($value) . '" ' . $selected . '>' . esc_html($label) . '</option>';
                         }
                     ?>
@@ -58,33 +64,24 @@
 
             <div class="brand fil-list sel-fil">
                 <h5>Car brand</h5>
-                <select name="brand" id="car-brand">
+                <select name="brand" id="brand">
                     <option value="">Any</option>
                     <?php 
                         $car_brands = array(
                             'toyota' => 'Toyota',
-                            'hyundai' => 'Hyundai',
+                            'honda' => 'Honda',
                             'proton' => 'Proton',
+                            'tesla' => 'Tesla',
+                            'perodua' => 'Perodua'
+
                         );
 
                         foreach ($car_brands as $brand_value => $brand_label) {
-                            $brand_selected = ($_GET['car-brand'] == $brand_value) ? 'selected' : '';
+                            $brand_selected = (isset($_GET['brand']) && $_GET['brand'] == $brand_value) ? 'selected' : '';
                             echo '<option value="' . esc_attr($brand_value) . '" ' . $brand_selected . '>' . esc_html($brand_label) . '</option>';
                         }
                     ?>
                 </select>
-            </div>
-
-            <div id="set-time-both">
-                <div class="from">
-                    <h5>From</h5>
-                    <input type="date" name="from" id="from" value="<?php echo esc_attr(isset($_GET['from']) ? $_GET['from'] : ''); ?>">
-                </div>
-
-                <div class="to">
-                    <h5>To</h5>
-                    <input type="date" name="to" id="to" value="<?php echo esc_attr(isset($_GET['to']) ? $_GET['to'] : ''); ?>">
-                </div>
             </div>
         </div>
 
@@ -105,25 +102,10 @@
         </div>
 
         <input type="name" class="search-field"
-    placeholder="<?php echo esc_attr_x( 'Search the car you want to rent', 'placeholder' ) ?>"
+    placeholder="<?php echo esc_attr_x( 'Search your location', 'placeholder' ) ?>"
     value="<?php echo get_search_query() ?>" name="s"
     title="<?php echo esc_attr_x( 'Search', 'label' ) ?>"
-    <?php
-    // Add the 'post_type' parameter to specify the custom post type(s) you want to search in
-    $custom_post_types = array('car'); // Replace with your custom post type slug(s)
-    $post_type_query = '';
-
-    if (!empty($_GET['post_type']) && in_array($_GET['post_type'], $custom_post_types)) {
-        $post_type_query = 'post_type=' . sanitize_text_field($_GET['post_type']);
-    } elseif (isset($_GET['post_type'])) {
-        // Handle invalid post_type values, if necessary
-    }
-
-    echo $post_type_query;
-    ?>
 />
-
-
         <button type="submit" class="search-button"
         title="<?php echo esc_attr_x( 'Search', 'label' ) ?>"
         ><i class="fa-solid fa-magnifying-glass"></i></button>
